9e3e492fe25d86969dc5e35f4eb7d9d76799e03f
MSM Mandal <msm74588@gmail.com>
2023-09-07 01:04:45 +0530

    last updated field not properly updated, but build.py working

--- build.py
+++ build.py
@@ -4,17 +4,22 @@ import yaml
 import hashlib
 import time
 import secrets  # Python built-in CSPRNG module
+import shutil
 from colorama import init, Fore, Style
 
 # Initialize Colorama for terminal color formatting
 init(autoreset=True)
 
+# Get the absolute path of the directory where this script is located
+script_directory = os.path.dirname(os.path.abspath(__file__))
+
 # Load settings from settings.yaml
-with open('settings.yaml', 'r') as settings_file:
+settings_file_path = os.path.join(script_directory, 'settings.yaml')
+with open(settings_file_path, 'r') as settings_file:
     settings = yaml.safe_load(settings_file)
 
 # Define the SHA-256 signature folder at the root level
-SIGNATURE_FOLDER = 'sha_signatures'
+SIGNATURE_FOLDER = os.path.join(script_directory, 'sha_signatures')
 
 # Define the regex pattern to match the fields
 field_patterns = {
@@ -48,15 +53,19 @@ def generate_sha256_signature(script_path):
 if not os.path.exists(SIGNATURE_FOLDER):
     os.makedirs(SIGNATURE_FOLDER)
 
-# List all script files in the "validated_scripts" folder
-validated_script_files = os.listdir("validated_scripts")
+# Get the absolute path of the "validated_scripts" folder
+validated_script_folder = os.path.join(script_directory, 'validated_scripts')
 
-# Initialize a dictionary to store the extracted information
-all_scripts_data = []
+# Ensure that the "validated_scripts" folder exists
+if not os.path.exists(validated_script_folder):
+    os.makedirs(validated_script_folder)
+
+# List all script files in the "validated_scripts" folder
+validated_script_files = os.listdir(validated_script_folder)
 
 # Loop through script files in the "validated_scripts" folder
 for script_file in validated_script_files:
-    script_path = os.path.join("validated_scripts", script_file)
+    script_path = os.path.join(validated_script_folder, script_file)
     signature_path = os.path.join(SIGNATURE_FOLDER, f"{script_file}.sha256")
 
     # Generate a unique passphrase for each script
@@ -69,11 +78,37 @@ for script_file in validated_script_files:
     with open(signature_path, 'w') as signature_file:
         signature_file.write(signature)
 
+# List all script files in the "scripts" folder
+script_files = os.listdir(os.path.join(script_directory, "scripts"))
+
+# Initialize a dictionary to store the extracted information
+all_scripts_data = []
+
+# Loop through script files in the "scripts" folder
+for script_file in script_files:
+    script_path = os.path.join(script_directory, "scripts", script_file)
+    validated_script_path = os.path.join(validated_script_folder, script_file)
+    signature_path = os.path.join(SIGNATURE_FOLDER, f"{script_file}.sha256")
+
+    # If the script does not exist in the validated_scripts folder, copy it
+    if not os.path.exists(validated_script_path):
+        shutil.copy2(script_path, validated_script_path)
+
+    # Generate a unique passphrase for each script
+    passphrase = generate_random_passphrase()
+
+    # Generate SHA-256 signature
+    signature = generate_sha256_signature(validated_script_path)
+
+    # Save the SHA-256 signature to a file at the root level
+    with open(signature_path, 'w') as signature_file:
+        signature_file.write(signature)
+
     # Initialize a dictionary to store the extracted information for this script
     extracted_data = {}
 
     # Read the script file
-    with open(script_path, 'r') as file:
+    with open(validated_script_path, 'r') as file:
         for line in file:
             for field_name, pattern in field_patterns.items():
                 match = re.match(pattern, line)
@@ -81,11 +116,11 @@ for script_file in validated_script_files:
                     extracted_data[field_name] = match.group(1)
 
     # Add the script and signature locations to the extracted data
-    extracted_data["script_location"] = f"https://example.com/scripts/{script_file}"
-    extracted_data["signature_location"] = f"https://example.com/signatures/{script_file}.sha256"
+    extracted_data["script_location"] = f"{settings['script_location_prefix']}{script_file}"
+    extracted_data["signature_location"] = f"{settings['signature_location_prefix']}{script_file}.sha256"
 
     # Add the "last_modified" field to the extracted data
-    last_modified = os.path.getmtime(script_path)
+    last_modified = os.path.getmtime(validated_script_path)
     extracted_data["last_modified"] = time.strftime("%b %d %Y, %I:%M:%S%p", time.localtime(last_modified))
 
     # Append the extracted data to the list
@@ -96,7 +131,8 @@ for script_file in validated_script_files:
 yaml_data = {str(i + 1): {k: v for k, v in script_data.items() if k != "passphrase"} for i, script_data in enumerate(all_scripts_data)}
 
 # Output the YAML data to resources.yml
-with open('resources.yml', 'w') as yaml_file:
+resources_file_path = os.path.join(script_directory, 'resources.yml')
+with open(resources_file_path, 'w') as yaml_file:
     yaml.dump(yaml_data, yaml_file, default_flow_style=False)
 
 print(f"[{Fore.GREEN}{get_current_time()}{Style.RESET_ALL}] Data has been saved to resources.yml.")
---
+++ scripts/template-test-3.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Title: Hello world
+# Description: Prints hello world
+# Version:
+# Verified: Yes
+# Author: MSM1234
+# Author Contact (optional): github.com/msm-linux
+# Last Updated: Sep 06 2023, 11:59:43PM
+
+echo "Hello world"
+echo "Hello world 2"
+echo "updated 11:59"
+echo "updated 12:53"
